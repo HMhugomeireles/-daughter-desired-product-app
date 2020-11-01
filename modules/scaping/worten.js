7316022f517432c332c3692ab1dbd1e5632bee7f
Meireles <meireles.hmm@gmail.com>
2020-11-01 01:57:56 +0000

    remove gitignore files

---
+++ modules/scaping/worten.js
@@ -0,0 +1,38 @@
+const puppeteer = require('puppeteer')
+const { autoScroll } = require('./util')
+
+function wortenSearchURL(product) {
+  return `https://www.worten.pt/search?query=${product}&sortBy=relevance&hitsPerPage=48&page=1`;
+}
+
+async function getProductFromWorten(product) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(wortenSearchURL(product))
+    await autoScroll(page)
+
+    const data = await page.evaluate(() => {
+      return Array.from(document.querySelector('#products-list-block').children)
+        .map(element => ({
+          img: element.querySelector('.lazy.loaded').src,
+          title: element.querySelector('.w-product__title').innerText,
+          priceTOShow: element.querySelector('.iss-current-price').innerText,
+          link: element.querySelector('a').href,
+          createAt: Date.now()
+        })
+        )
+    })
+
+    await browser.close();
+    return data;
+
+  } catch (error) {
+    throw new Error("[Worten scrapping] ->", error)
+  }
+}
+
+module.exports = {
+  getProductFromWorten
+}
