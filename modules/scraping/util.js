272f739c7329715ee06be29e5e3e43525aba0f96
Meireles <meireles.hmm@gmail.com>
2020-11-01 00:59:40 +0000

    refactoring the worten module

---
+++ modules/scraping/util.js
@@ -0,0 +1,22 @@
+async function autoScroll(page) {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      var totalHeight = 0;
+      var distance = 100;
+      var timer = setInterval(() => {
+        var scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
+
+module.exports = {
+  autoScroll
+}
