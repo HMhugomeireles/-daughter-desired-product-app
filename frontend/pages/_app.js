ed48674c9609b472e68021512eefd79831d510ba
Hugo Meireles <hugo.meireles@edirectinsurance.com>
2020-11-20 08:55:45 +0000

    insert wrap layout component to app top level

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css'
 
 import { Provider } from 'next-auth/client';
+import Layout from '../components/container/layout';
 
 const App = ({ Component, pageProps }) => {
   const { session } = pageProps;
   return (
     <Provider options={{ site: process.env.SITE }} session={session}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </Provider>
   );
 };
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,14 +2,10 @@
 import LoginContainer from '../components/container/login'
 
 
+
 export default function Index() {
 
-
-
-
   return (
-    <div>
       <LoginContainer />
-    </div>
   )
 }
