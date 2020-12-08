4d4ba065b51d543c88eeb72bccf2dd27ff53fd26
Hugo Meireles <meireles.hmm@gmail.com>
2020-12-08 23:56:07 +0000

    create wallet page

---
+++ frontend/pages/dashboard/wallet.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/container/layout'
+
+export default function Wallet() {
+    return (
+        <Layout>
+            <div>Wallet</div>
+        </Layout>
+    )
+}
